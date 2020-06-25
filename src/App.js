0fc9c5273462d992295414e214eac6d8d146fd6d
Liam McAteer <liam@liammcateer.nz>
2020-06-25 21:25:17 +1200

    added prticle background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+import particleConfig from './assets/particleConfig.json';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World.</h1>
+      <Particles className="particle-container" params={particleConfig}/>
+      <h1>Hello World</h1>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navigation"></nav>
+            </div>
+        )
+    }
+}
+
+export default Navigation
