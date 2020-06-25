6081ee85c78a8b7c52e0249a0ad52752861b3541
Liam McAteer <liam@liammcateer.nz>
2020-06-25 17:44:20 +1200

    added old site to react template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Liam McAteer</h1>
+        <hr />
+        <h3>Site is currently under construction.</h3>
+        <p>Email: <a href="mailto:liam@liammcateer.nz">liam@liammcateer.nz</a></p>
+        <p>Linked In: <a href="https://www.linkedin.com/in/liam-mcateer">linkedin.com/in/liam-mcateer</a></p>
     </div>
   );
 }
