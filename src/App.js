39097b2fd20a2b7081e3acc390d37f98baf1c12e
Liam McAteer <liam@liammcateer.nz>
2020-06-25 19:25:52 +1200

    react cleanup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Liam McAteer</h1>
-        <hr />
-        <h3>Site is currently under construction.</h3>
-        <p>Email: <a href="mailto:liam@liammcateer.nz">liam@liammcateer.nz</a></p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/liam-mcateer">linkedin.com/in/liam-mcateer</a></p>
+      <h1>Hello, World.</h1>
     </div>
   );
 }
