8245cb9237979c3a377a8d8f89e48c15b08502bc
Liam McAteer <liam@liammcateer.nz>
2020-08-06 22:00:22 +1200

    Added typewriter effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Typist from 'react-typist';
+import TypistCSS from 'react-typist/dist/Typist.css';
 import config from '../assets/config.json';
 
 export class Home extends Component {
@@ -8,10 +10,14 @@ export class Home extends Component {
                 <div className="home-wrapper">
                     <h1>
                         Hi,<br />
-                        I'm <a href={config.linkedin} target="_blank" rel="noopener noreferrer" className="accent">{config.name}</a>
+                        I'm <a href={config.linkedin} target="_blank" rel="noopener noreferrer" className="accent">
+                                {config.name}
+                            </a>
                     </h1>
                     <div className="home-skills">
-                        {config.skills.map((skill) => {return <p>{skill}</p>})}
+                        <Typist cursor={{element: '‎ _'}}>
+                            {config.skills.map((skill) => {return <span className="skill">{skill}<br></br></span>})}
+                        </Typist>
                     </div>
                 </div>                
             </div>
