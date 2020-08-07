a1fa906d19a6c4680353ffd6c65398a4c72cfcb8
Liam McAteer <liam@liammcateer.nz>
2020-08-08 11:36:10 +1200

    added navigation tweaks

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function scrollToSection(sectionID){
+function scrollToSection(sectionID) {
+    toggleNav();
     let section = document.getElementById(sectionID);
-    section.scrollIntoView({behavior: 'smooth'});
+    section.scrollIntoView({ behavior: 'smooth' });
 }
 
-function toggleNav(){
-    let mainNav = document.getElementById('main-nav');
-    if(mainNav.style.transform == 'translateY(0px)'){
-        mainNav.style.transform = 'translateY(-110%)';
-    }else{
-        mainNav.style.transform = 'translateY(0px)'
+function toggleNav() {
+    if (window.innerWidth <= 576) {
+        let mainNav = document.getElementById('main-nav');
+        if (mainNav.style.transform == 'translateY(0px)') {
+            mainNav.style.transform = 'translateY(-110%)';
+        } else {
+            mainNav.style.transform = 'translateY(0px)'
+        }
     }
 }
