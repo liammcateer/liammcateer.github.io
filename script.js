6c9e7d97421f8d150dad09ad3c430d652dd4cdc0
Liam McAteer <liam@liammcateer.nz>
2020-08-08 13:09:01 +1200

    added contact form

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ function toggleNav() {
         }
     }
 }
+
+function processContactForm(form){
+    //Extract form data
+    let formElements = form.elements;
+    let formData = {
+        name: formElements['name'].value,
+        email: formElements['email'].value,
+        message: formElements['message'].value,
+    }
+
+    // Send contact form
+    let formStatusMessage = document.getElementById('formStatusMessage');
+    formStatusMessage.innerHTML = "Sending ...";
+
+    axios.post('https://liammcateer-profile-site.herokuapp.com/api/contact', formData)
+        .then((response) => {
+            formStatusMessage.innerHTML = "Your message has been sent.";
+            formStatusMessage.classList.remove('error')
+            formStatusMessage.classList.add('success')
+        })
+        .catch((error) => {
+            formStatusMessage.innerHTML = "There was an error sending your message. In the meantime you can contact me via email:" + 
+                "<br><a href='mailto:liam@liammcateer.nz'>liam@liammcateer.nz</a>";
+            formStatusMessage.classList.remove('success')
+            formStatusMessage.classList.add('error')
+
+        });
+
+    return false;
+
+}
